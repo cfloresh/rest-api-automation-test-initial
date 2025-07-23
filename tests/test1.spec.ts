4f3dcc33f6214303bc95c29c49b969df80eb040c
Cristhian Ivan Flores Hurtado <cfloresh@emeal.nttdata.com>
2025-07-23 11:52:23 -0500

    primer commit pruebas apis banco internacional

---
+++ tests/test1.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect, request } from '@playwright/test';
+
+/*en esta constante vamos a definir la*/
+const bodyRequest = {
+  id: 20102,
+  username: "tesq",
+  firstName: "tesq",
+  lastName: "tesq",
+  email: "tesq",
+  password: "tesq",
+  phone: "tesq",
+  userStatus: 0
+} /*aqui no es necesario que las propiedades esten en comilla*/
+
+
+/*aqui la estructura basica del test*/
+test('Crear una nueva mascota', async ({ request }) => {
+  /*ejecutamos el servicio con el await y capturamos la respuesta en la constante para luego imprimirla*/
+  const respuesta = await request.post('https://petstore.swagger.io/v2/user', {
+    data: bodyRequest,
+    ignoreHTTPSErrors: true  // Agregado para ignorar errores de HTTPS
+
+  })
+
+  console.log(JSON.stringify(await respuesta.json())) /*IMPRIMO POR CONSOLA LA RESPUESTA */
+
+
+})
