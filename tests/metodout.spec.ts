73367792a58f28ef28fa808f05b8186205ccecf4
Cristhian Ivan Flores Hurtado <cfloresh@emeal.nttdata.com>
2025-07-24 16:12:53 -0500

    agregar test metodo put

---
+++ tests/metodout.spec.ts
@@ -0,0 +1,41 @@
+import { test, expect, request } from '@playwright/test';
+
+/*metodo get */
+test('CONSULTAR USUARIO ACTUALIZAR',{tag : '@USOPUT'}, async ({ request }) => {
+  const listarMascota = await request.get('https://petstore.swagger.io/v2/user/test4test4', {
+    ignoreHTTPSErrors: true 
+  })
+  console.log(JSON.stringify(await listarMascota.json())) 
+  const var1 = await listarMascota.json()
+})
+
+/*Metodo Put*/
+test('ACTUALIZAR USUARIO POR ID',{tag :'@USOPUT'}, async ({ request }) => {
+  const updatauSERRequest = {
+  id: 10120200,
+  username: "test5test5",
+  firstName: "test5test5",
+  lastName: "test5test5",
+  email: "test5test5",
+  password: "test5test5",
+  phone: "test5test5",
+  userStatus: 0
+}
+
+   await request.put('https://petstore.swagger.io/v2/user/test4test4', {
+    data: updatauSERRequest,
+    ignoreHTTPSErrors: true 
+  })
+
+})
+
+
+
+/*metodo get */
+test('CONSULTAR USUARIO ACTUALIZADO',{tag : '@USOPUT'}, async ({ request }) => {
+  const listarMascota = await request.get('https://petstore.swagger.io/v2/user/test5test5', {
+    ignoreHTTPSErrors: true 
+  })
+  console.log(JSON.stringify(await listarMascota.json())) 
+  const var1 = await listarMascota.json()
+})
